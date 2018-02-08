c09fc47a3b6eaeb0d785ba7c346830a3d9c0d5b8
leftovers491 <Leftovers491@gmail.com>
2018-02-07 16:00:05 -0800

    test

--- app/src/main/java/com/example/darrell/haulhelpers/MainActivity.java
+++ app/src/main/java/com/example/darrell/haulhelpers/MainActivity.java
@@ -10,7 +10,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-//TEST
+//TEST1
 
 public class MainActivity extends AppCompatActivity {
     private Button mDriver, mCustomer;
