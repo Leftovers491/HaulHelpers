8013c9f062f6bed20efc2e4974ec0edee3f853e0
Keviinmorales <keviinmorales@gmail.com>
2018-03-23 13:20:21 -0700

    Act 29 - From History Open - Information on Ride

--- app/src/main/java/com/example/darrell/haulhelpers/historyRecyclerView/HistoryViewHolders.java
+++ app/src/main/java/com/example/darrell/haulhelpers/historyRecyclerView/HistoryViewHolders.java
@@ -1,9 +1,12 @@
 package com.example.darrell.haulhelpers.historyRecyclerView;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.TextView;
 
+import com.example.darrell.haulhelpers.HistorySingleActivity;
 import com.example.darrell.haulhelpers.R;
 
 /**
@@ -25,6 +28,12 @@ public class HistoryViewHolders extends RecyclerView.ViewHolder implements View.
 
     @Override
     public void onClick(View v) {
+
+        Intent intent = new Intent(v.getContext(), HistorySingleActivity.class);
+        Bundle b = new Bundle();
+        b.putString("rideId", rideId.getContext().toString());
+        intent.putExtras(b);
+        v.getContext().startActivity(intent);
 
     }
 }
