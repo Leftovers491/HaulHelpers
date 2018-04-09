643b91abc9241e6b1c15e0a782674f21c7de6b4e
013416011 <leftovers491@gmail.com>
2018-04-09 16:18:03 -0700

    Act 36 Getting Paypal to HaulHelpers Setup

---
+++ app/src/main/java/com/example/darrell/haulhelpers/PayPalConfig.java
@@ -0,0 +1,11 @@
+package com.example.darrell.haulhelpers;
+
+/**
+ * Created by 013416011 on 4/9/18.
+ */
+
+public class PayPalConfig {
+
+    public static final String PAYPAL_CLIENT_ID = "ASQM_J28xyRrqKeYriehFevpFh4Qz_CHxejRS2EQdisi9ZfHai1ER8kmgVcp_YlQamyYPWefebJCpGlM";
+
+}
